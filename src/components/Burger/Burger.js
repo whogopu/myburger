02044ba1a61fbb01bf1535d1d8816f5eeb056ea4
Gopal sharma <gopal151295@gmail.com>
2018-03-20 09:04:07 +0530

    added controls and pricing

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,28 @@
+import React from "react";
+import classes from "./Burger.css";
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
+
+const burger = props => {
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, elem) => {
+      return arr.concat(elem);
+    }, []);
+
+    if(transformedIngredients.length <= 0)
+    transformedIngredients = <div>Please add ingredients...</div>
+
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+};
+
+export default burger;
