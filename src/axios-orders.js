e2c641414d07d918f5020f033202bc3b5be13ee8
Gopal sharma <gopal151295@gmail.com>
2018-03-31 08:20:23 +0530

    redux setup

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
-import axios from "axios";
+import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: "https://react-my-burger-151295.firebaseio.com/"
+    baseURL: 'https://react-my-burger-151295.firebaseio.com/'
 });
 
 export default instance;
